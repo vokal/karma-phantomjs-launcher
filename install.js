ce5b5d9fadc5439b1d96933870c662f004fe8a5c
Jarrett Widman <jarrett@24m2.com>
2015-02-16 10:24:06 -0600

    writing location file

--- install.js
+++ install.js
@@ -1,11 +1,17 @@
 var whereis = require( "whereis" );
 var fs = require( "fs" );
+var path = require( "path" );
 
 
 function writeLocationFile( err, location )
 {
+    location = location.replace("\n", "").replace("\r", "");
+    if( path.sep === "\\" )
+    {
+        location = location.replace( /\\/g, "\\\\" );
+    }
     fs.writeFileSync( path.join( __dirname, "location.js" ),
-        "module.exports.location = '" + location + "'" );
+        "module.exports.location = '" + location + "';" );
 }
 
 whereis( "phantomjs", writeLocationFile );
