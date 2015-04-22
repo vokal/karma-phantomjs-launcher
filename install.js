33623e9660093edb5c8e757eea5acf3e495052a2
Jarrett Widman <jarrett@24m2.com>
2015-04-22 09:53:02 -0500

    print an error when phantom is not found in path

--- install.js
+++ install.js
@@ -1,10 +1,18 @@
 var whereis = require( "whereis" );
 var fs = require( "fs" );
 var path = require( "path" );
+var chalk = require( "chalk" );
 
 
 function writeLocationFile( err, location )
 {
+    if( err )
+    {
+        console.log(chalk.red( err ));
+        console.log(chalk.yellow( "If phantomjs is not in PATH, install phantomjs and then install karma-phantomjs-launcher-path again" ));
+        return;
+    }
+
     location = location.replace("\n", "").replace("\r", "");
     if( path.sep === "\\" )
     {
